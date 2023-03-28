2e9c6126f32d2708331efaa114a5d55f9b53128e
larnik35 <larnik35@mail.ru>
2023-03-28 22:36:59 +0300

    add Task_5

--- ДЗ1/Task_5.java
+++ ДЗ1/Task_5.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 //Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69. Требуется восстановить выражение до верного равенства. 
 //Предложить хотя бы одно решение или сообщить, что его нет. @Liberate520
 
-
-
 public class Task_5 {
     public static void main(String[] args) {
         System.out.println(" Привет мы проверяем выражение x? + ?y = z");
@@ -14,7 +12,7 @@ public class Task_5 {
         Scanner iScanner = new Scanner(System.in);
 
         System.out.println("Введите десяток 1-го числа (x)");
-        Integer x = iScanner.nextInt() *10;
+        Integer x = iScanner.nextInt() * 10;
 
         System.out.println("Введите единицу 2-го  числа (y) ");
         Integer y = iScanner.nextInt();
@@ -22,17 +20,17 @@ public class Task_5 {
         System.out.println("Введите сумму (sum)");
         Integer sum = iScanner.nextInt();
 
-        for (int i = 0; i < 100; i = i +10) {
+        for (int i = 0; i < 100; i = i + 10) {
             for (int j = 0; j < 10; j++) {
-                if ((x+j) + (i + y) == sum) {
-                    System.out.println("Востанавливаю   " + (x+j) + " + " +(i + y) + " = " + sum );
-                    return ;
-                }                
+                if ((x + j) + (i + y) == sum) {
+                    System.out.println("Востанавливаю   " + (x + j) + " + " + (i + y) + " = " + sum);
+                    return;
+                }
             }
-            
+
         }
         System.out.println("Ничего не нашли (");
-        
+
     }
-    
+
 }
